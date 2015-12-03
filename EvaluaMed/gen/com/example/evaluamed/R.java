08c9407963219657fcc0299125fef06865633158
Adriana Cortés Soto <adri925@gmail.com>
2015-12-02 23:21:49 -0600

    icon image

--- EvaluaMed/gen/com/example/evaluamed/R.java
+++ EvaluaMed/gen/com/example/evaluamed/R.java
@@ -26,6 +26,8 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
         public static final int info=0x7f020002;
         public static final int infobutton=0x7f020003;
+        public static final int play=0x7f020004;
+        public static final int playstoreicon=0x7f020005;
     }
     public static final class id {
         public static final int BtnLoggin=0x7f08000b;
